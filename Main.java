674db02afea14217fb995dac991c4778a4abb3e6
Chitrashri Gond <chitrashri.xworkz@gmail.com>
2021-04-28 09:18:51 +0530

    first java file

---
+++ Main.java
@@ -0,0 +1,38 @@
+public class Main {
+  public static void main(String[] args) {
+
+    byte range;
+    range = 126;
+    System.out.println(range);  
+ 
+    short range1;
+    range1 = 6000;
+    System.out.println(range1);
+
+    int range2;
+     range2 = 4350000;
+System.out.println(range2);
+
+long range3;
+range3 = 4532967812l;
+System.out.println(range3);
+
+float myFloatNum;
+myFloatNum = 40.000000f;
+System.out.println(myFloatNum);
+
+  double range4;
+     range4 = 25.000;
+System.out.println(range4);
+
+char Var;
+     Var = 'c';
+System.out.println(Var);
+
+boolean Variable;
+     Variable = true;
+System.out.println(Variable);
+
+
+  }
+}
